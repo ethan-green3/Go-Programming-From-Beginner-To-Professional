f00289c9540680720fce3d00d280196b6433e6ef
ethan-green3 <EPG200000@utdallas.edu>
2025-11-12 12:48:41 -0600

    Activity 1.01
    Signed-off-by: ethan-green3 <EPG200000@utdallas.edu>

---
+++ Part 1 - Scripts/Chapter 1 - Variables and Operators/Activity 1.01 - Defining and Printing/go.mod
@@ -0,0 +1,3 @@
+module Activity_1.01
+
+go 1.24.2
---
+++ Part 1 - Scripts/Chapter 1 - Variables and Operators/Activity 1.01 - Defining and Printing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Ethan"
+	familyName := "Green"
+	age := 24
+	peanutAllergy := false
+
+	fmt.Println(firstName)
+	fmt.Println(familyName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
