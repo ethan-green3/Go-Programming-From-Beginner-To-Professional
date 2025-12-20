a466d51294021bc81c80ef6e8710f0d672e4d911
ethan-green3 <EPG200000@utdallas.edu>
2025-12-20 10:04:07 -0600

    Exercise 9.03

---
+++ Part 3 - Modules/Chapter 9 - Using Go Modules to Define a Project/Exercise 9.03 - Working with workspaces/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ethan-green3/printer"
+)
+
+func main() {
+	msg := printer.PrintNewUUID()
+	fmt.Println(msg)
+}
---
+++ Part 3 - Modules/Chapter 9 - Using Go Modules to Define a Project/Exercise 9.03 - Working with workspaces/printer/go.mod
@@ -0,0 +1,5 @@
+module github.com/ethan-green3/printer
+
+go 1.24.1
+
+require github.com/google/uuid v1.6.0
