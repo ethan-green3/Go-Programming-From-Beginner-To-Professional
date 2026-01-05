ec44bb850fd816d90cba14190760e1eb1b0e8a01
ethan-green3 <EPG200000@utdallas.edu>
2026-01-05 15:24:49 -0600

    Exercise 17.01

---
+++ Part 5 - Building for the Web/Chapter 17 - Using the Go HTTP Client/Exercise 17.01 - Sending a GET Request/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func getDataAndReturnResponse() string {
+	r, err := http.Get("https://google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func main() {
+	data := getDataAndReturnResponse()
+	log.Println(data)
+}
